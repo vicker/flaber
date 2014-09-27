46d148477ac38f07e356806252d7253b138f859c
vicker <vicker@gmail.com>
2014-09-28 00:14:37 +0800

    Added comment for the default admin password
    [update] Added comment for the default admin password

--- src/function/password.php
+++ src/function/password.php
@@ -1,1 +1,7 @@
-<? $password = "fa8ec2abba0ca86f9b4c7ed667381753"; ?>
+<?
+/*
+this is the default admin password "flaber" in MD5
+please be reminded to change the password after deployment
+*/
+$password = "fa8ec2abba0ca86f9b4c7ed667381753";
+?>
